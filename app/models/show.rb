2109d9bf8da3fb0fc7d55d7297f70a52b8e5c558
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-01 22:36:15 -0400

    commit tree dd2233e08842d993394e0313ca352f86de5bd63c

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 class Show < ActiveRecord::Base
+
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        list = []
+        for thespian in self.actors do
+            list << thespian.full_name
+            #binding.pry
+        end
+        list
+    end
   
 end
