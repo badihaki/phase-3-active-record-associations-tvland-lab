4e21b23f2808fce271952de575fe589d1740f00e
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-01 22:36:26 -0400

    assignment completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 class Actor < ActiveRecord::Base
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        roles = []
+        for char in self.characters do
+            role = "#{char.name} - #{char.show.name}"
+            #binding.pry
+            roles << role
+        end
+        roles
+    end
   
 end
